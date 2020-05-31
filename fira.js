05f22ce30be63d5cf6e404aac94e9889e463319c
John-Philip Johansson <john-philip.johansson@episerver.com>
2020-05-31 16:39:17 +0200

    First working version
    Only celebrates Nikola Tesla (10th of July) 😁
    
    Base website code copied from my fika website:
    https://johansson.jp/fika

---
+++ fira.js
@@ -0,0 +1,41 @@
+document.fira = {};
+
+document.fira.kalender = {
+    // January
+    0: {
+    },
+    // February
+    1: {
+    },
+    // March
+    2: {
+    },
+    // April
+    3: {
+    },
+    // May
+    4: {
+    },
+    // June
+    5: {
+    },
+    // July
+    6: {
+        10: [{ name: "Nikola Tesla", year: 1856 }],
+    },
+    // August
+    7: {
+    },
+    // September
+    8: {
+    },
+    // October
+    9: {
+    },
+    // November
+    10: {
+    },
+    // December
+    11: {
+    }
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+const today = new Date();
+const month = today.getMonth();
+const date = today.getDate();
+
+const firaDate = document.fira.kalender[month][date];
+
+const $fira = document.getElementById("fira-node");
+
+$fira.innerHTML = firaDate ? `<h3>${firaDate[0].name}<h3>` : "<h3>dig själv!</h3>";
