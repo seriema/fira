6b757894c9a5458780221fa09e40b5011463ae99
John-Philip Johansson <john-philip.johansson@episerver.com>
2020-05-31 16:50:25 +0200

    Support grabbing an image from Wikimedia
    If the birthday person has a Wikipedia page and a suitable, it will
    be displayed.

--- index.js
+++ index.js
@@ -6,4 +6,26 @@ const firaDate = document.fira.kalender[month][date];
 
 const $fira = document.getElementById("fira-node");
 
-$fira.innerHTML = firaDate ? `<h3>${firaDate[0].name}</h3>` : "<h3>dig själv!</h3>";
+// Celebrate someone's birthday
+if (firaDate) {
+    // Pick the first birthday person for now
+    const fira = firaDate[0];
+
+    $fira.innerHTML = `<h3>${fira.name}</h3>`;
+
+    // Try to get a picture of the birthday person :)
+    const imageHeight = 300;
+    const wikimediaUrl = `https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=${fira.name}&prop=pageimages&format=json&pithumbsize=${imageHeight}`;
+
+    fetch(wikimediaUrl, { mode: 'cors' })
+        .then(response => response.json())
+        .then(data => {
+            const pageKeys = Object.keys(data.query.pages);
+            const imageUrl = data.query.pages[pageKeys[0]].thumbnail.source;
+            $fira.innerHTML += `<img src="${imageUrl}" />`
+        });
+}
+// Fall back to celebrate yourself <3
+else {
+    $fira.innerHTML = "<h3>dig själv!</h3>";
+}
