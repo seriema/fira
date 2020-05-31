931a99172977c9e839bdc4dd67c38c9f52234982
John-Philip Johansson <john-philip.johansson@episerver.com>
2020-05-31 21:57:37 +0200

    Output how many birthday dates and birthdays we have
    Useful to keep track of how many days until we have all 365 (366)
    days covered.

--- kalender.js
+++ kalender.js
@@ -2,6 +2,8 @@ const today = new Date();
 
 const fira = document.fira.kalender;
 const $fira = document.getElementById("fira-node");
+let birthdayDates = 0;
+let birthdays = 0;
 
 Object.keys(fira).forEach((month) => {
     const $monthHeader = document.createElement("h3");
@@ -10,7 +12,9 @@ Object.keys(fira).forEach((month) => {
 
     const $month = document.createElement("ul");
     Object.keys(fira[month]).forEach((date) => {
+        birthdayDates++;
         fira[month][date].forEach((firaDate) => {
+            birthdays++;
             const $date = document.createElement("li");
 
             $date.innerHTML = `${date}: `;
@@ -21,3 +25,8 @@ Object.keys(fira).forEach((month) => {
 
     $fira.appendChild($month);
 });
+
+// Worth keeping track of how many days of the year there are birthdays
+const $birthdays = document.createElement("p");
+$birthdays.innerText = `${birthdayDates} dagar om året har vi ${birthdays} bra anledningar att äta tårta!`;
+$fira.appendChild($birthdays);
